ede8363fbd35240fdb276703d91bad06c11bc6f3
Ayushvidh <ayushvidhale099@gmail.com>
2022-11-02 08:28:57 +0530

    second commit

--- src/main/java/com/springdemogatiki/demo/DemoApplication.java
+++ src/main/java/com/springdemogatiki/demo/DemoApplication.java
@@ -3,19 +3,8 @@ package com.springdemogatiki.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
-@RestController
 public class DemoApplication {
-
-	@GetMapping("/message")
-	public String message(){
-		return "Congo!! Spring Boot project deplopoyed successfully...."
-	}
-	@GetMapping("/")
-	public String message(){
-		return "This is your root webpage...."
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
